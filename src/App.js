db79e8d148766b54b7da5288e9a62740960b61e0
krjordan02 <keeganrjordan@gmail.com>
2024-02-03 22:09:41 -0700

    Completed Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { Fragment, useEffect } from 'react';
+import React from 'react';
+
+
+function Post({ title, summary, img, url}){
+  return (
+    <div className='post-container'>
+      <a href={url}><img className='post-img' src={img}/></a>
+      <p className='post-title' id='title'>{title}</p>
+      <p className='post-summary' id='summary'>{summary}</p>
+    </div>
+  )
+}
 
 function App() {
+  const [loaded, setLoaded] = React.useState(false);
+  const [posts, setPosts] = React.useState();
+
+  useEffect(() => {
+    axios.get(`https://api.spaceflightnewsapi.net/v4/articles/`)
+      .then(data => {
+        const articles = [];
+        data.data.results.forEach(element => {
+          let temp = {title: element.title, summary: element.summary, img: element.image_url, url: element.url};
+          articles.push(temp);
+        });
+        setPosts(articles);
+        setLoaded(true);
+      })
+  }, [])
+  
+
   return (
+    <Fragment>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Space News</h1>
+      <ul>
+        {loaded ? posts.map(post => {           
+          return(
+          <li key={post.title} className='post'>
+            <Post title={post.title} summary={post.summary} img={post.img} url={post.url} key={0}></Post>
+          </li>)
+        }) : console.log("loading...")}
+      </ul>
     </div>
+    </Fragment>
   );
 }
 
